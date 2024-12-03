c8c690f75e17acc888a575a882e4183f884de887
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-12-03 14:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+let form = document.getElementById("form");
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+
+form.addEventListener('submit', (e)=>{
+	e.preventDefault();
+
+	let nameInput = name.value.trim();
+	let ageInput = age.value.trim();
+
+	if(!nameInput || !ageInput){
+		alert("both fields are required");
+		return;
+	}
+
+	let ageNum = parseInt(ageInput, 10);
+
+	let promise = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(ageNum > 18){
+			resolve();
+		}else{
+			reject();
+		}
+		},4000)
+	});
+
+	promise.then(() =>{
+		alert(`Welcome ${nameInput}. You can vote`);
+	}).catch(()=>{
+		alert(`Oh sorry ${nameInput}. You aren't old enough`);
+	 });
+
+});
